3739f424070f2ff9ea7594172f6bd3976993aa7f
Tareq Alsadaqa <tareq.alsadaqa1@gmail.com>
2025-03-16 20:21:44 +0300

    First commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ezy_Skills.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
